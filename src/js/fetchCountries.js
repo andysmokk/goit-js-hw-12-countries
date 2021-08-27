aa7f5fe0eb5c4c5da5bf33220f112d37fab11e4f
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-08-27 10:46:16 +0200

    async/await

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,12 +2,10 @@ import onFetchError from './fetchError';
 
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
-function fetchCountry(country) {
-  return fetch(`${BASE_URL}/name/${country}`)
-    .then(response => {
-      return response.json();
-    })
-    .catch(onFetchError);
+async function fetchCountry(country) {
+  const response = await fetch(`${BASE_URL}/name/${country}`);
+  const dataResponse = await response.json();
+  return dataResponse;
 }
 
 export default { fetchCountry };
